b6913f137ebe165728042fd314e0ca480a2b6999
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-10 23:01:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-
+// 1,1,2,3,5,8
 int main() {
-   return 0;
+    int n = 0;
+    cout << "Inserire un numero maggiore o uguale a 1" << endl;
+    cin >> n;
+
+    if (n>=1){
+        int k = 1;
+        int a = 1;
+        int b = 1;
+        int c = a + b;
+        cout << a << endl;
+        cout << b << endl;
+        while (k < n){
+            k++;
+            cout << c << endl;
+            a = c - a;
+            b = c;
+            c = a + b;
+        }
+    }else{
+        cout << "errore" << endl;
+    }
+    return 0;
 }
